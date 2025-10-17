a40a4857d308b0dd0325a8eb4475b5b8080eb172
DAVID ANTONIO PALMA FLORES <156796608+palmasd@users.noreply.github.com>
2025-10-17 10:05:40 +0200

    subiendo mi carpteta a mi repositorio

---
+++ widgets/creaClases.bat
@@ -0,0 +1,13 @@
+
+echo ""> label.py
+echo ""> checkbox.py
+echo ""> combobox.py
+echo ""> datetimeedit.py
+echo ""> groupbox.py
+echo ""> linedit.py
+echo ""> pushbutton.py
+echo ""> radiobuttons.py
+echo ""> slider.py
+echo ""> tabwidget.py
+echo ""> textedit.py
+
---
+++ widgets/init_.py
@@ -0,0 +1,6 @@
+from . import label, checkbox, combobox, datetimeedit, dial
+from . import groupbox, linedit, pushbutton, radiobuttons, slider, tabwidget, textedit
+
+
+__all__ = ["label", "checkbox", "combobox", "datetimeedit", "dial", "groupbox", "linedit", "pushbutton", "radiobuttons", "slider", "tabwidget", "textedit"]
+
