04a54163eca5e0fd3cec3da508f4949ebac12cfe
DAVID ANTONIO PALMA FLORES <156796608+palmasd@users.noreply.github.com>
2025-10-17 10:32:09 +0200

    subiendolos a una misma carpeta

---
+++ pyside6_widgets_demo/widgets/label.py
@@ -0,0 +1,18 @@
+from PySide6.QtWidgets import QGroupBox, QVBoxLayout, QLabel, QCheckBox
+from PySide6.QtCore import Qt
+
+def create( parent=None):
+    box= QGroupBox("Mi label")
+    layout = QVBoxLayout(box)
+
+    label = QLabel("hola, este es mi Qlabel")
+    label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+    
+    info_label = QLabel("Señales: -Ninguna | Metodos: setTex(str), setAlignment(...)")
+    info_label.setStyleSheet("color: gray; font-size: 10pt;")
+
+    layout.addWidget(label)
+    layout.addWidget(info_label)
+
+    return box
+
